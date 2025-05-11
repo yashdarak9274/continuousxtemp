fddd7860930611aa6eab677149319b6e433fe2b1
yashdarak9274 <ygd205@nyu.edu>
2025-05-11 15:36:14 -0400

    first commit

---
+++ tf/kvm/data.tf
@@ -0,0 +1,37 @@
+data "openstack_networking_network_v2" "sharednet2" {
+  name = "sharednet2"
+}
+
+data "openstack_networking_subnet_v2" "sharednet2_subnet" {
+  name = "sharednet2-subnet"
+}
+
+data "openstack_networking_secgroup_v2" "allow_ssh" {
+  name = "allow-ssh"
+}
+
+data "openstack_networking_secgroup_v2" "allow_9001" {
+  name = "allow-9001"
+}
+
+data "openstack_networking_secgroup_v2" "allow_8000" {
+  name = "allow-8000"
+}
+
+data "openstack_networking_secgroup_v2" "allow_8080" {
+  name = "allow-8080"
+}
+
+data "openstack_networking_secgroup_v2" "allow_8081" {
+  name = "allow-8081"
+}
+
+data "openstack_networking_secgroup_v2" "allow_http_80" {
+  name = "allow-http-80"
+}
+
+data "openstack_networking_secgroup_v2" "allow_9090" {
+  name = "allow-9090"
+}
+
+
